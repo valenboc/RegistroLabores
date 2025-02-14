715fff83800f368c297346289dbecb02d6c4aff1
valenboc <144183321+valenboc@users.noreply.github.com>
2025-02-14 10:34:52 -0500

    error solution

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LaborController;
 
-Route::get('/', [LaborController::class, 'components.labors.index'])->name('labors.index');
+Route::get('/', [LaborController::class, 'index'])->name('labors.index');
 Route::resource('labors', LaborController::class);
